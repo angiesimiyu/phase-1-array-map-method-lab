fa28e99fa376b1d6864a24a4ddeed30e4b0204e9
angelinesimiyu <angie.lukelas@gmail.com>
2022-12-10 10:05:26 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+
+
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(element => element.split(" ").map(word => word[0].toUpperCase()+word.slice(1)).join(" "))
 }
